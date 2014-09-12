cb8021c1ba5b3dcabb1f01a4e1af41e0d03fbd4b
telehan <wanghansky@gmail.com>
2014-09-12 13:07:42 +0800

    add glibtoolize for mac osx

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,14 @@
 #! /bin/sh
 
+case `uname` in 
+Darwin*) 
+  alias cmd_libtoolize=glibtoolize ;;
+*) 
+  alias cmd_libtoolize=libtoolize ;;
+esac
+
 bootstrap() {
-  libtoolize -c && \
+  cmd_libtoolize -c && \
   aclocal -I config && \
   automake -a -c && \
   autoconf
